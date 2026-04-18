8e9b391f7633debfabdfe6ae186c35ccb1bf3136
Jona <jonamehmeti4@gmail.com>
2026-04-18 20:08:45 +0200

    Funksioni per Permissions
    Deklarimi i funksionit hasPermission i cili kontrollon se a ka useri leje per me pas access ne file.

---
+++ server.js
@@ -0,0 +1,30 @@
+const dgram = require("dgram");
+const { handleCommand } = require("./fileCommands");
+
+const server = dgram.createSocket("udp4");
+
+const SERVER_IP = "127.0.0.1";
+const SERVER_PORT = 4444;
+
+const users = {
+  admin1: ["read", "write", "execute"],
+  guest1: ["read"],
+  guest2: ["read"],
+  guest3: ["read"],
+};
+
+function hasPermission(username, permission) {
+  return users[username] && users[username].includes(permission);
+}
+
+server.on("message", (msg, rinfo) => {
+  let data;
+
+  try {
+    data = JSON.parse(msg.toString());
+  } catch {
+    return server.send("Invalid JSON", rinfo.port, rinfo.address);
+  }
+
+  const { username, command } = data;
+});
